3a8b3b126ea4097eaa6d387f176d958f3af19ad4
Xing Zhao <zhaoxing8325@gmail.com>
2022-03-16 23:45:19 -0700

    async let demo

--- AysncLetDemo/ViewController.swift
+++ AysncLetDemo/ViewController.swift
@@ -12,8 +12,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadImages()
+        //loadImagesAsyncLet()
     }
 
+    // MARK: Async Let
+    
+    func loadImage(index: Int) async -> UIImage {
+        let imageURL = URL(string: "https://picsum.photos/200/300")!
+        let request = URLRequest(url: imageURL)
+        let (data, _) = try! await URLSession.shared.data(for: request)
+        print("finished loading image \(index)")
+        return UIImage(data: data)!
+    }
 
+    // we tell the application to wait for the first image to be returned until it can continue to fetch the second image
+    func loadImages() {
+        Task {
+            let firstImage = await loadImage(index: 1)
+            let secondImage = await loadImage(index: 2)
+            let thirdImage = await loadImage(index: 3)
+            let images = [firstImage, secondImage, thirdImage]
+            print("images are here \(images)")
+        }
+    }
+    
+    // download images in parallel
+    func loadImagesAsyncLet() {
+        Task {
+            async let firstImage = loadImage(index: 4)
+            async let secondImage =  loadImage(index: 5)
+            async let thirdImage = loadImage(index: 6)
+            let images = await [firstImage, secondImage, thirdImage]
+            print("images are here \(images)")
+        }
+    }
 }
 
